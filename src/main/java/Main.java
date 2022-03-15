42cda40cd2b074c8093debbcee2afe334e07c89d
TeddyGuy <mediteddyguy@gmail.com>
2022-03-14 20:13:13 -0400

    Creation des exigences du travail dans Main [TEST]

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        //AdminService adminService = new AdminService();
+        //adminService.saveClient("John Doe", "Passw0rd123");
+        //adminService.saveDocument("Le Torrent","Anne Hébert","Tragédie",1950,"Bibliothèque Québecoise",164);
+        //List<Document> documentResults = adminService.findDocument("torrent","Anne Hébert","Tragédie",1950);
+        //System.out.println(documentResults);
+        //ClientService clientService = new ClientService("John Doe", "Passw0rd123");
+        //clientService.borrowDocumentById(results.get(0));
+        //List<DocumentLoan> documentLoanResults = clientService.findAllDocumentLoan();
+        //System.out.println(documentLoanResults);
+    }
+}
