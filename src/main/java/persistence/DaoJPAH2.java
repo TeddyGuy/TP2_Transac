23943c425c7616ef10c35fb3f31f43616ba7734e
TeddyGuy <mediteddyguy@gmail.com>
2022-03-14 20:46:25 -0400

    Implementation de la methode DaoJPAH2.save

--- src/main/java/persistence/DaoJPAH2.java
+++ src/main/java/persistence/DaoJPAH2.java
@@ -8,6 +8,12 @@ public abstract class DaoJPAH2<T> implements Dao<T> {
     private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("hibernate");
     @Override
     public void save(T t) {
+        final EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
 
+        em.persist(t);
+
+        em.getTransaction().commit();
+        em.close();
     }
 }
