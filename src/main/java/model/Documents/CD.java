a0a038ca13a027b04457e9d28bc7bbf19b6870e0
TeddyGuy <mediteddyguy@gmail.com>
2022-03-15 01:01:56 -0400

    Creation de la classe CD

---
+++ src/main/java/model/Documents/CD.java
@@ -0,0 +1,18 @@
+package model.Documents;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@DiscriminatorValue("CD")
+public class CD extends Document{
+    public final static int BORROW_TIME_IN_WEEK = 2;
+}
