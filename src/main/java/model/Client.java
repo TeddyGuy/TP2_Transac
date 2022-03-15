4c55cd535b0793011376787a925403a353de2afd
TeddyGuy <mediteddyguy@gmail.com>
2022-03-14 20:23:11 -0400

    Creation de la classe Client

---
+++ src/main/java/model/Client.java
@@ -0,0 +1,23 @@
+package model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String username;
+    private String password;
+}
--- src/main/java/service/AdminService.java
+++ src/main/java/service/AdminService.java
@@ -3,10 +3,14 @@ package service;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import model.Client;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class AdminService {
-
+    //Dao<Client> clientDAO;
+    public void saveClient(String username, String password) {
+        //clientDAO.save(username,password);
+    }
 }
